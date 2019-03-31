3e17b4d9820bd862b272b3d16285ce9c1227ace2
GGL12 <aka.guo712@gmail.com>
2019-03-31 13:21:54 +0800

    model

--- model.py
+++ model.py
@@ -19,3 +19,21 @@ def FillMean(data_s):
     '''输入series数据，返回填充均值的series'''
 
     return data_s.fillna(data_s.mean())
+
+
+def FillNa(data):
+    '''
+       预测排名2.
+       删除排名2为空的数据.
+       价格1、星级、评论数量 使用均值填充.
+       价格2 填充对应的价格1.
+    '''
+
+    data = data[data.排名2.notna()].reset_index(drop=True)
+    data.价格1 = FillMean(data.价格1)
+    data.星级 = FillMean(data.星级)
+    data.评论数量 = FillMean(data.评论数量)
+    indexes = np.where(np.isnan(data.价格2))[0]
+    data.loc[indexes, '价格2'] = data.loc[indexes, '价格1']
+
+    return data
